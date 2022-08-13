e05e21755834d44f56af0dfae48e279cdf8caefa
Alexis Sahagun <alexissahagunitj7a@gmail.com>
2022-08-13 14:38:11 -0500

    fixed mobile responsiveness issues in homepage

--- components/Beneficios.jsx
+++ components/Beneficios.jsx
@@ -2,12 +2,12 @@ import React from 'react'
 import BeneficiosText from '../components/BeneficiosContent'
 import { isBot } from 'next/dist/server/web/spec-extension/user-agent'
 
-const Beneficios = ({heading, subheading}) => {
+const Beneficios = () => {
     return (
-       <div className="grid grid-cols-2 justify-items-center mt-10 md:mx-[348px]  align-middle">
+       <div className="grid md:grid-cols-2 grid-cols-1 justify-items-center mt-10 md:mx-[348px] align-middle">
                {BeneficiosText && BeneficiosText.map((item) =>
-                <div className="flex flex-row items-center my-[39px] mx-[30px]">
-                    <img src={item.icon} alt="" className="p-[27px]"/>
+                <div className="flex flex-row items-center md:my-[39px] my-[30px] md:mx-[30px] mx-6">
+                    <img src={item.icon} alt="" className="md:p-[27px] pr-2 md:pr-0"/>
                     <div className="flex flex-col text-left space-y-[15px]">
                         <h6 className="font-semibold stella-orange uppercase text-base leading-[18px]">{item.heading}</h6>
                         <p className="font-medium text-[13px] tracking-normal">{item.body}</p>
--- pages/Soluciones.jsx
+++ pages/Soluciones.jsx
@@ -24,13 +24,13 @@ const Soluciones = () => {
 
             <VideoComponent video="video2.mp4" size="md:h-[660px] h-[300px]"/>
 
-            <div className="text-center flex flex-col items-center md:mx-[420px] mx-8 space-y-5">
+            <div className="text-center flex flex-col items-center md:mx-[420px] space-y-5 mx-4">
                 <div className="flex flex-row items-center">
-                    <img src="LineThroughHeading.png" alt="" className="mx-8"/>
-                    <h2 className="uppercase stella-orange font-semibold text-[24px]">Beneficios para su industria o negocio</h2>
-                    <img src="LineThroughHeading.png" alt="" className="mx-8"/>
+                    <img src="LineThroughHeading.png" alt="" className="mx-8 hidden md:block"/>
+                    <h2 className="uppercase stella-orange font-semibold md:text-[24px] text-lg">Beneficios para su industria o negocio</h2>
+                    <img src="LineThroughHeading.png" alt="" className="mx-8 hidden md:block"/>
                 </div>
-                <p className="font-medium text-gray-600 text-[18px] leading-10">Conozca algunos de los beneficios que tendrá por trabajar con paneles solares.</p>
+                <p className="font-medium text-gray-600 md:text-[18px] text-base leading-10">Conozca algunos de los beneficios que tendrá por trabajar con paneles solares.</p>
             </div>
 
             <Beneficios/>
