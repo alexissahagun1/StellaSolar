0655f8526240cd970aa89587d2ad4e62355e894f
Alexis Sahagun <alexissahagunitj7a@gmail.com>
2022-08-14 14:02:46 -0500

    name change fix

--- components/CarouselComponent.jsx
+++ components/CarouselComponent.jsx
@@ -6,7 +6,7 @@ import { Carousel } from 'react-responsive-carousel';
 const CarouselComponent = ({height}) => {
     return (
         <div className="flex flex-col items-center md:mt-[40px] md:mx-[500px] m-6 ">
-           <Carousel showThumbs={false} autoPlay interval={7000} infiniteLoop>
+           <Carousel showThumbs={false} autoPlay interval={7000} infiniteLoop showStatus={false} showIndicators={false}>
                 {carouselImages && carouselImages.map((item) =>
                         <div key={item.id}>
                             <img src={item.image} alt="" className={`object-cover ${height} rounded-xl md:rounded-none `}/>
