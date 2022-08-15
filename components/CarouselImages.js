54f069f856a5d622f3400c1d8d9be4cb8ce58e9e
Alexis Sahagun <alexissahagunitj7a@gmail.com>
2022-08-15 13:24:08 -0500

    fixed path issue

--- components/CarouselImages.js
+++ components/CarouselImages.js
@@ -1,53 +1,53 @@
 const carouselImages = [
     {
         id: 1,
-        image: '/fotos/Image5.JPG',
+        image: '/Fotos/Image5.JPG',
         
     },
     {
         id: 2,
-        image: '/fotos/Image11.JPG',
+        image: '/Fotos/Image11.JPG',
         
     },
     {
         id: 3,
-        image: '/fotos/Image13.JPG',
+        image: '/Fotos/Image13.JPG',
         
     },
     {
         id: 4,
-        image: '/fotos/Image14.JPG',
+        image: '/Fotos/Image14.JPG',
 
         
     },
     {
         id: 5,
-        image: '/fotos/Image15.JPG',
+        image: '/Fotos/Image15.JPG',
         
     },
     {
         id: 6,
-        image: '/fotos/Image19.JPG',
+        image: '/Fotos/Image19.JPG',
         
     },
     {
         id: 7,
-        image: '/fotos/Image37.JPG',
+        image: '/Fotos/Image37.JPG',
         
     },
     {
         id: 8,
-        image: '/fotos/Image34.JPG',
+        image: '/Fotos/Image34.JPG',
         
     },
     {
         id: 9,
-        image: '/fotos/Image31.JPG',
+        image: '/Fotos/Image31.JPG',
         
     },
     {
         id: 10,
-        image: '/fotos/Image25.JPG',
+        image: '/Fotos/Image25.JPG',
         
     }
 ]
