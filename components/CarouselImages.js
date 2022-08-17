628301b97333e4125e2931e002d5647601ad46dc
Alexis Sahagun <alexissahagunitj7a@gmail.com>
2022-08-16 22:24:03 -0500

    added new photos to the proyectos destacados carousel

--- components/CarouselImages.js
+++ components/CarouselImages.js
@@ -1,12 +1,12 @@
 const carouselImages = [
     {
         id: 1,
-        image: '/Fotos/Image5.JPG',
+        image: '/Fotos/Image44.JPG',
         
     },
     {
         id: 2,
-        image: '/Fotos/Image11.JPG',
+        image: '/Fotos/Image45.JPG',
         
     },
     {
@@ -16,7 +16,7 @@ const carouselImages = [
     },
     {
         id: 4,
-        image: '/Fotos/Image14.JPG',
+        image: '/Fotos/Image16.JPG',
 
         
     },
@@ -31,13 +31,23 @@ const carouselImages = [
         
     },
     {
+        id: 20,
+        image: '/Fotos/Image46.jpg',
+        
+    },
+    {
+        id: 19,
+        image: '/Fotos/Image14.jpg',
+        
+    },
+    {
         id: 7,
         image: '/Fotos/Image37.JPG',
         
     },
     {
         id: 8,
-        image: '/Fotos/Image34.JPG',
+        image: '/Fotos/Image38.JPG',
         
     },
     {
@@ -55,6 +65,59 @@ const carouselImages = [
         image: '/Fotos/Image43.jpg',
         
     },
+    {
+        id: 12,
+        image: '/Fotos/Image5.jpg',
+        
+    },
+    {
+        id: 13,
+        image: '/Fotos/Image11.jpg',
+        
+    },
+    {
+        id: 21,
+        image: '/Fotos/Image47.jpg',
+        
+    },
+    {
+        id: 22,
+        image: '/Fotos/Image48.jpg',
+        
+    },
+    {
+        id: 23,
+        image: '/Fotos/Image49.jpg',
+        
+    },
+    {
+        id: 14,
+        image: '/Fotos/Image10.jpg',
+        
+    },
+    {
+        id: 15,
+        image: '/Fotos/Image27.jpg',
+        
+    },
+    {
+        id: 16,
+        image: '/Fotos/Image32.jpg',
+        
+    },
+    {
+        id: 17,
+        image: '/Fotos/Image36.jpg',
+        
+    },
+    {
+        id: 18,
+        image: '/Fotos/Image34.jpg',
+        
+    },
+   
+
+    
 ]
 
 export default carouselImages; 
