8590b230c1ea75dfdf72a89bafaf58faf95fd208
alexissahagun1 <cesar.sahagun@oracle.com>
2023-09-22 19:49:48 -0600

    bug fix 2

--- components/HeroVideo.jsx
+++ components/HeroVideo.jsx
@@ -1,10 +1,17 @@
 import React from 'react'
-
+import Image from 'next/image'  // Import the Image component
 
 const Hero = ({image, heading, message, message2, size}) => {
     return (
-        <div className="flex flex-col ">
-            <img src={image} className={`object-cover ${size} w-full`}/>
+        <div className="flex flex-col relative">  {/* Added relative positioning */}
+            <div className={`relative ${size} w-full`}>  {/* Wrapped Image with a div and applied styles */}
+                <Image 
+                    src="/HeroImage.jpg "
+                    alt="Hero Image" 
+                    layout="fill" 
+                    objectFit="cover"  
+                />
+            </div>
             <div className="text-white absolute flex flex-col mt-[290px] 2xl:mx-96 lg:mx-32 md:mx-20 mx-4 mb-56 xl:w-5/12 md:w-7/12">
                 <h2 className="md:text-4xl text-2xl font-medium tracking-tight pb-10">{heading}</h2>
                 <p className="font-bold leading-9 md:text-2xl text-xl stella-orange">{message}</p>
