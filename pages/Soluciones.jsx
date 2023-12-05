dfb4a249417b5fe81f206ad14b167a2d7799937f
alexissahagun1 <cesar.sahagun@oracle.com>
2023-12-04 20:06:10 -0600

    added new banner to home page

--- pages/Soluciones.jsx
+++ pages/Soluciones.jsx
@@ -23,7 +23,8 @@ const Soluciones = () => {
     return (
         <div>
             <Title/>
-            <HeroVideo video="heroVideo.mp4" heading="Paneles solares para hogares, comercios e industria." message="1 Año de poliza de mantenimiento gratis" size="md:h-[789px] h-[600px]"/>
+            {/* <HeroVideo video="heroVideo.mp4" heading="Paneles solares para hogares, comercios e industria." message="1 Año de poliza de mantenimiento gratis" size="md:h-[789px] h-[600px]"/> */}
+            <HeroVideo video="heroVideo.mp4" heading="" message="" size="md:h-[789px] h-[600px]"/>
 
             <div className="text-center mt-[104px] flex flex-col items-center xl:mx-[420px] mx-8">
                 <h2 className="uppercase stella-orange font-semibold text-[24px] mb-8">Soluciones llave en mano para tus necesidades</h2>
